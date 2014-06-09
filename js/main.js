7a0e618f9be9800a60f05f40ec405cf5d7ca9211
aagricola <amandaagricola@gd-36e5c0.mica.edu>
2014-06-09 18:46:02 -0400

    made parralax pentagon

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function () {
 	// code here
+	//the class 'has-parallax' is made up, can be any name you want
+	$('html').addClass('has-parallax');
+	//simplest way to target is by using an id - #scene, .parallax is the method or function
+	$('#scene').parallax();
+	
 });
