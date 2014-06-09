2a9f20550d4cf3092000a64340d7d584ffc09c84
aagricola <amandaagricola@gd-36e5c0.mica.edu>
2014-06-09 19:05:57 -0400

    scalarX

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ $(function () {
 	//the class 'has-parallax' is made up, can be any name you want
 	$('html').addClass('has-parallax');
 	//simplest way to target is by using an id - #scene, .parallax is the method or function
-	$('#scene').parallax();
+	$('#scene').parallax({
+		scalarX: 2,
+		scalarY: 8
+	});
 	
 });
